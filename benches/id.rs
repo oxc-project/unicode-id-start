d0ab8e55bc03bf60178688fbb7d9f0ce48bca94f
Boshen <boshenc@gmail.com>
2025-10-07 20:00:36 +0800

    refactor: replace wrapping_shr with direct bit shift (#10)

--- benches/id.rs
+++ benches/id.rs
@@ -52,13 +52,13 @@ fn gen_string(p_nonascii: u32) -> String {
 fn bench(c: &mut Criterion, group_name: &str, string: String) {
     let mut group = c.benchmark_group(group_name);
     group.measurement_time(Duration::from_secs(10));
-    group.bench_function("baseline", |b| {
-        b.iter(|| {
-            for ch in string.chars() {
-                black_box(ch);
-            }
-        });
-    });
+    // group.bench_function("baseline", |b| {
+    // b.iter(|| {
+    // for ch in string.chars() {
+    // black_box(ch);
+    // }
+    // });
+    // });
     group.bench_function("unicode-id-start", |b| {
         b.iter(|| {
             for ch in string.chars() {
@@ -67,43 +67,43 @@ fn bench(c: &mut Criterion, group_name: &str, string: String) {
             }
         });
     });
-    group.bench_function("unicode-id", |b| {
-        b.iter(|| {
-            for ch in string.chars() {
-                black_box(unicode_id::UnicodeID::is_id_start(ch));
-                black_box(unicode_id::UnicodeID::is_id_continue(ch));
-            }
-        });
-    });
-    group.bench_function("ucd-trie", |b| {
-        b.iter(|| {
-            for ch in string.chars() {
-                black_box(trie::ID_START.contains_char(ch));
-                black_box(trie::ID_CONTINUE.contains_char(ch));
-            }
-        });
-    });
-    group.bench_function("fst", |b| {
-        let id_start_fst = fst::id_start_fst();
-        let id_continue_fst = fst::id_continue_fst();
-        b.iter(|| {
-            for ch in string.chars() {
-                let ch_bytes = (ch as u32).to_be_bytes();
-                black_box(id_start_fst.contains(ch_bytes));
-                black_box(id_continue_fst.contains(ch_bytes));
-            }
-        });
-    });
-    group.bench_function("roaring", |b| {
-        let id_start_bitmap = roaring::id_start_bitmap();
-        let id_continue_bitmap = roaring::id_continue_bitmap();
-        b.iter(|| {
-            for ch in string.chars() {
-                black_box(id_start_bitmap.contains(ch as u32));
-                black_box(id_continue_bitmap.contains(ch as u32));
-            }
-        });
-    });
+    // group.bench_function("unicode-id", |b| {
+    // b.iter(|| {
+    // for ch in string.chars() {
+    // black_box(unicode_id::UnicodeID::is_id_start(ch));
+    // black_box(unicode_id::UnicodeID::is_id_continue(ch));
+    // }
+    // });
+    // });
+    // group.bench_function("ucd-trie", |b| {
+    // b.iter(|| {
+    // for ch in string.chars() {
+    // black_box(trie::ID_START.contains_char(ch));
+    // black_box(trie::ID_CONTINUE.contains_char(ch));
+    // }
+    // });
+    // });
+    // group.bench_function("fst", |b| {
+    // let id_start_fst = fst::id_start_fst();
+    // let id_continue_fst = fst::id_continue_fst();
+    // b.iter(|| {
+    // for ch in string.chars() {
+    // let ch_bytes = (ch as u32).to_be_bytes();
+    // black_box(id_start_fst.contains(ch_bytes));
+    // black_box(id_continue_fst.contains(ch_bytes));
+    // }
+    // });
+    // });
+    // group.bench_function("roaring", |b| {
+    // let id_start_bitmap = roaring::id_start_bitmap();
+    // let id_continue_bitmap = roaring::id_continue_bitmap();
+    // b.iter(|| {
+    // for ch in string.chars() {
+    // black_box(id_start_bitmap.contains(ch as u32));
+    // black_box(id_continue_bitmap.contains(ch as u32));
+    // }
+    // });
+    // });
     group.finish();
 }
 
